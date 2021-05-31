1cf14acc5ee249fdeed9a05bfef1ed574ee1b273
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-05-31 17:27:29 +0000

    Update QA.md

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,3 +1,7 @@
 import { Application } from "@hotwired/stimulus"
 
-const application 
+const application = Application.start()
+
+// Configure Stimulus development experience.
+application.debug = false
+window.Stimulus   = application
