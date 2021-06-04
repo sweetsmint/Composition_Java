416d8249a3df708cf8f25cf45cbb030ba7837f19
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-06-04 06:12:47 +0000

    clarify

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,4 +2,6 @@ class ApplicationJob < ActiveJob::Base
   # Automatically retry jobs that encountered a deadlock
   # retry_on ActiveRecord::Deadlocked
 
-  # Most jobs ar
+  # Most jobs are safe to ignore if the underlying records are no longer available
+  # discard_on ActiveJob::DeserializationError
+end
