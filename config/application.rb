afe7c953131212d2ade0a46f0f6794e9abb586ed
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-07-24 12:38:35 +0000

    remove a warning

--- config/application.rb
+++ config/application.rb
@@ -2,4 +2,6 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*
