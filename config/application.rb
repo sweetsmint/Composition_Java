a3f2602498f74767359f860ca209543bf5287c34
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-07-31 14:06:08 +0000

    Update Motor.py

--- config/application.rb
+++ config/application.rb
@@ -28,4 +28,7 @@ module Hello
 
     # Mount Action Cable outside the main process or domain.
     config.action_cable.mount_path = nil
-    config.action_cable.url = ENV.fetch("ACTION_CABLE_FRONTEND_URL") { "ws://localhos
+    config.action_cable.url = ENV.fetch("ACTION_CABLE_FRONTEND_URL") { "ws://localhost:28080" }
+
+    # Only allow connections to Action Cable from these domains.
+    origins = ENV.fetch("ACTION_CABLE_ALLOWED_REQUEST_ORIGINS
