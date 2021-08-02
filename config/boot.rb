8a6f82aa466b9483107432f50c05fd04b84d68a9
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-08-02 20:56:08 +0000

    Update doc comment

--- config/boot.rb
+++ config/boot.rb
@@ -4,4 +4,4 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 require "bundler/setup"
 
 # Speed up boot time by caching expensive operations, but not in production.
-requir
+require "bootsnap/setup" if Gem.loaded_specs.has_key?("bootsnap")
