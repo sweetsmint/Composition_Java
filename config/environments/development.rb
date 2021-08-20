040e421a3b9ed812aa8476e00251978b8dad67ea
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-08-20 13:33:54 +0000

    Pep8.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,4 +30,7 @@ Rails.application.configure do
   else
     config.action_controller.perform_caching = false
 
-    config.cache_sto
+    config.cache_store = :null_store
+  end
+
+  # Store uploaded files on the local file system (see config/storage.yml for option
