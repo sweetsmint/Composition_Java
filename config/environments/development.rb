109d2aad3072df514e28cf63b5dc28754df6adfd
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-08-28 21:05:26 +0000

    fix ga

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,9 @@ Rails.application.configure do
   config.active_support.disallowed_deprecation_warnings = []
 
   # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_l
+  config.active_record.migration_error = :page_load
+
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
+
+  # Disable digesti
