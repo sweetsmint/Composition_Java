1a6a61f05b9f132b0cae4e88200aacb39a70787b
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-09-02 22:38:18 +0000

    Delete login.html

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,7 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
-  # Uncomment if you wish to allow Action Cable access fr
+  # Uncomment if you wish to allow Action Cable access from any origin.
+  # config.action_cable.disable_request_forgery_protection = true
+
+  # Comma separated list of IP addresses / CIDRs that 
