f990d81ed20103ef40140ca5628158ce0b7cb038
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-09-06 21:37:25 +0000

    update ADK

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified 
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # Code is not reloaded between requests.
+  config.cache_classes = true
+
+  
