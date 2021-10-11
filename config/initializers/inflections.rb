38acbb9a6df0c7acb7e88930d0c8bf5489b08312
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-10-11 07:03:50 +0000

    cv updated

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,4 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Add new inflection rules using the following format. Inflections
-# are locale specific, and
+# are locale specific, and you may define rules for as many different
+# locales as you wish. All of these examples are active by default:
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+# 
