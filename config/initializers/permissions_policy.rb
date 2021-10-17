9307db3f9bc081d0051daa599edb8a6806227b63
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-10-17 16:38:10 +0000

    corrected a typo

--- config/initializers/permissions_policy.rb
+++ config/initializers/permissions_policy.rb
@@ -2,4 +2,9 @@
 # information see https://developers.google.com/web/updates/2018/06/feature-policy
 #
 # Rails.application.config.permissions_policy do |f|
-#   f.camera  
+#   f.camera      :none
+#   f.gyroscope   :none
+#   f.microphone  :none
+#   f.usb         :none
+#   f.fullscreen  :self
+#   f.payment     :self, "https://secur
