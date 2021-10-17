17760ba9c67f4e14ddac11aa04011b367116a8b3
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-10-17 07:15:00 +0000

    Internal update.

--- config/initializers/permissions_policy.rb
+++ config/initializers/permissions_policy.rb
@@ -1,2 +1,5 @@
 # Define an application-wide HTTP permissions policy. For further
-# inform
+# information see https://developers.google.com/web/updates/2018/06/feature-policy
+#
+# Rails.application.config.permissions_policy do |f|
+#   f.camera  
