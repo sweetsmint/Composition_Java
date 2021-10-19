cb074b30253881a86a2d329285d88ee23bb6c123
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-10-19 18:03:07 +0000

    Updated the TODO file.

--- config/puma.rb
+++ config/puma.rb
@@ -3,4 +3,6 @@ bind "tcp://0.0.0.0:#{ENV.fetch("PORT") { "8000" }}"
 environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setti
+# The `threads` method setting takes two numbers: a minimum and maximum.
+# Any libraries that use thread pools should be configured to match
+# the maximum value specified for Puma. Defa
