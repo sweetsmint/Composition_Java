cc2f107e504a5ec481a250be48c79554d2bfe540
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-10-19 15:52:24 +0000

    fs: Workaround the compiler's bad optimization

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,6 @@
 # Specify the bind host and environment.
 bind "tcp://0.0.0.0:#{ENV.fetch("PORT") { "8000" }}"
-environment ENV.f
+environment ENV.fetch("RAILS_ENV") { "production" }
+
+# Puma can serve each request in a thread from an internal thread pool.
+# The `threads` method setti
