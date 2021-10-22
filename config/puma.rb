64d02fa82682ca2dedf28950d5586118b75be465
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-10-22 14:11:11 +0000

    fixed notice

--- config/puma.rb
+++ config/puma.rb
@@ -12,4 +12,6 @@ threads threads_count, threads_count
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
-# the concurrency of the application would be m
+# the concurrency of the application would be max `threads` * `workers`.
+# Workers do not work on JRuby or Windows (both of which do not support
+# processes). It defaults to the num
