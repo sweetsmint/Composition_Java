2ae5ace59691155c510cab22e6f094e3ea72143f
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-10-27 11:11:54 +0000

    * copied library into demo projects * updated library information (debug output in console)

--- config/puma.rb
+++ config/puma.rb
@@ -22,4 +22,6 @@ workers = ENV.fetch("WEB_CONCURRENCY") { Etc.nprocessors * 2 }
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to firs
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavio
