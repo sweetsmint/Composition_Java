a4eb88778af526e9ce928a5b0194c109bbd76f0b
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-10-30 18:55:34 +0000

    net: qmi_wwan: add ZTE MF60

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get "/up/databases", to: "up#databases", as: :up_databases
 
   # Sidekiq has a web dashboard which you can enable below. It's turned off by
-  # default because you very likely w
+  # default because you very likely wouldn't want this to be available to
+  # everyone in production.
+  #
+  # Uncomment the 2 lines below to enable the dashboard W
