50b4b29165d770de113c24ffd878b483f32cf189
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-10-31 16:35:28 +0000

    tcp: xps: fix reordering issues

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   # default because you very likely wouldn't want this to be available to
   # everyone in production.
   #
-  # Uncomment the 2 lines below to enable the dashboard W
+  # Uncomment the 2 lines below to enable the dashboard WITHOUT authentication,
+  # but be careful because even anonymous web visitors will be able to see it!
+  # require "sid
