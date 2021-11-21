bfd3fa56cd1123d5ca4adb544ded32b16a5fc3cd
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2021-11-21 14:30:34 +0000

    Update doc block

--- db/schema.rb
+++ db/schema.rb
@@ -2,4 +2,6 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `bi
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error 
