819544ef157226b0cfa91215fb4a6f960cd68cdb
sweetsmint <107674774+sweetsmint@users.noreply.github.com>
2022-01-03 10:35:22 +0000

    TTY: pmac_zilog, check existence of ports in pmz_console_init()

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,4 +1,11 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  tes
+  test "should get home" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should get up" do
+    get up_url
+    asser
